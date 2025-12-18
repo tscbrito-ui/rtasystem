4ce036502f8bc33c6070a06c7c31f8680b5191c5
Thaynara Brito <tscbrito@gmail.com>
2025-12-18 12:58:37 -0300

    chore: ignorar eslint no build da vercel

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        pathname: '/photos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        pathname: '/workspace-*/image/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'replicate.delivery',
-        pathname: '/**',
-      },
-    ],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
